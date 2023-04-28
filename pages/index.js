1e03e58840099017b17a54b648c55a91235dc255
jcruzv-prog <jesus.cruz.verdecia@gmail.com>
2023-04-28 16:38:15 -0500

    Modified Layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+import Layout from '../components/layout';
 export default function Home(){
   return(
-  <div>
-      <h1>Hello from NextJs!</h1>
-    </div>
-  )
+<Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Landing on Next Material</title>
+      </Head>
+        <h1>Simple Landing on Next Js and Material</h1>
+      <Button variant='contained'>Material Click</Button>
+    </Layout>
+  );
 }
