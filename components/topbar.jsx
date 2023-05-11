6a18b669e475a649d8c5e51e9c688c714fb10897
jcruzv-prog <jesus.cruz.verdecia@gmail.com>
2023-05-11 12:32:03 -0500

    Initial layout

---
+++ components/topbar.jsx
@@ -0,0 +1,26 @@
+import { Box, AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import {Menu as MenuIcon} from '@mui/icons-material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+export default function TopBar(){
+    return (
+       <Box sx={{display:'flex' }}>
+        <AppBar position='static' sx={{borderRadius:2}}>
+          <Toolbar>
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h5" sx={{ml:2, flexGrow:1}} noWrap>Amazing Store Products</Typography>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+              >
+                <AccountCircle />
+              </IconButton>
+              </Toolbar>
+
+        </AppBar>
+       </Box>
+    )
+}
---
+++ pages/dashboard.jsx
@@ -0,0 +1,17 @@
+
+import Head from 'next/head'; 
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Layout from '../components/layout';
+export default function DashBoard(){
+
+  return( 
+    <Layout >
+    <Typography component="h1" variant='h4'>User DashBoard</Typography>
+    </Layout>
+  )
+}
+
